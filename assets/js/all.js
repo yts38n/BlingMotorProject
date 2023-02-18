7ef88e6b2d07bcdc6d3b1ba0042810af3a523a07
yts38n <nyt9338@gmail.com>
2023-02-18 18:06:56 +0800

    Update 2023-02-18T10:06:53.261Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -216,7 +216,7 @@ var blingMotorUserStatus = {
 var signoutBtn = document.querySelector('#singoutControl');
 signoutBtn.addEventListener('click', doSignout);
 function getCalendar(newQueryYear, newQueryMonth) {
-  axios.get("http://localhost:3000/api/v1/customers/calendar", {
+  axios.get("https://bling-motor-mock-server.onrender.com/api/v1/customers/calendar", {
     params: {
       'year': newQueryYear.toString(),
       'month': newQueryMonth.toString()
